fb8f51831f3a1fc382076dbc8b31ebde9777b832
Ari <ariane.jurado@kairosds.com>
2020-01-28 16:41:00 +0100

    set vuetify

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,23 +1,16 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <!-- <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> -->
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="app">
+    <v-content>
+      <router-view />
+    </v-content>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  name: 'App',
+  data: () => ({
+    //
+  })
 }
-body{
-  margin: 0;
-}
-
-</style>
+</script>
--- front/src/main.js
+++ front/src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 Vue.prototype.$http = axios
 
 new Vue({
   router,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
